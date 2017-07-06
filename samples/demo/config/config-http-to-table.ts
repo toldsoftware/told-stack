4f2a1bf376e16d7522cf0adea589211f22bc9ac8
Rick Love <rick@toldpro.com>
2017-07-06 18:16:26 -0500

    Add Http to Table

---
+++ samples/demo/config/config-http-to-table.ts
@@ -0,0 +1,4 @@
+import { Config } from "@told/stack/lib/data-patterns/http-to-table/src-config/config";
+
+export const config = new Config();
+
---
+++ told-stack-npm/src/data-patterns/http-to-table/src-server/function-01-http.ts
@@ -0,0 +1,54 @@
+import { HttpFunction_Config, HttpFunction_TemplateConfig, OutputQueueData, HttpFunction_BindingData } from "../src-config/config";
+import { HttpFunctionResponse, HttpFunctionRequest } from "../../../core/types/functions";
+
+// Http Request: Handle Update Request
+// Blob In: Read Old Lookup Blob Value
+// Queue Out: Update Request Queue
+// Http Response: Return Old Lookup Value with Short TTL
+
+export function createFunctionJson(config: HttpFunction_TemplateConfig) {
+    return {
+        bindings: [
+            {
+                name: "req",
+                type: "httpTrigger",
+                direction: "in",
+                authLevel: "anonymous",
+                route: config.http_route
+            },
+            {
+                name: "res",
+                type: "http",
+                direction: "out"
+            },
+            {
+                name: "outOutputQueue",
+                type: "queue",
+                direction: "out",
+                tableName: config.outputTable_tableName,
+                partitionKey: config.outputTable_partitionKey,
+                rowKey: config.outputTable_rowKey,
+                connection: config.outputTable_connection
+            },
+        ],
+        disabled: false
+    };
+}
+
+export function runFunction(config: HttpFunction_Config, context: {
+    log: typeof console.log,
+    done: () => void,
+    res: HttpFunctionResponse,
+    bindingData: HttpFunction_BindingData,
+    bindings: {
+        outOutputQueue: OutputQueueData,
+    }
+}, req: HttpFunctionRequest) {
+    const data = config.getDataFromRequest(req, context.bindingData);
+    context.bindings.outOutputQueue = data;
+    // context.log('The Data was Queued', data);
+    context.res = {
+        body: 'The Data was Queued'
+    };
+    context.done();
+};
